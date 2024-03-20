fc682507c1b8eb5e1e82c4e359b2edd9d63c517b
Plufin <118924716+Plufin@users.noreply.github.com>
2024-03-20 19:49:52 +0200

    Add coloreScheme

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ThemeProvider } from './hooks/ThemeContext';
+import { COLORS } from './constants/theme';
 import Habits from './screens/Habits';
 import Home from './screens/Home';
 import Settings from './screens/Settings';
@@ -10,15 +12,17 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Habits'
-      >
-        <Stack.Screen name='Habits' component={Habits} />
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Settings' component={Settings} />
-        <Stack.Screen name='ToDo' component={ToDo} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Habits'
+        >
+          <Stack.Screen name='Habits' component={Habits} />
+          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Settings' component={Settings} />
+          <Stack.Screen name='ToDo' component={ToDo} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
