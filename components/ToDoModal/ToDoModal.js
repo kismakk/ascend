8e250bd4d22978f434e9a1e8dc270d35bb5a3716
Jaakko Kauppila <76628873+Kaubbila@users.noreply.github.com>
2024-04-07 16:40:03 +0300

    Forms

--- components/ToDoModal/ToDoModal.js
+++ components/ToDoModal/ToDoModal.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import { View, Button, Modal, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Modal, Text, SafeAreaView, TextInput, Pressable, TouchableOpacity, StyleSheet } from 'react-native';
 import { useTheme } from "../../hooks/ThemeContext";
 import getDynamicStyles from './ToDoModal.styles';
-import styles from './ToDoModal.styles';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, Controller } from 'react-hook-form';
 
 const ToDoModal = ({ todoModalVisible, setToDoModalVisible }) => {
 
   const { theme } = useTheme();
   const styles = getDynamicStyles(theme);
+  const [calendarVisible, setCalendarVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [dateText, setDateText] = useState('');
 
+  const toDoSchema = yup.object().shape({
+    title: yup.string().required('Title is required'),
+    difficulty: yup.string().required('Difficulty is required'),
+    date: yup.string().required('Date is required'),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue
+  } = useForm({
+    resolver: yupResolver(toDoSchema),
+    defaultValues: {
+      title: '',
+      notes: '',
+      difficulty: '',
+      dueDate: ''
+    },
+  });
+  
+  const calendarAction = (e, selectedDate) => {
+    if (e.type === "dismissed") { 
+      setCalendarVisible(false); 
+    } else {
+      setCalendarVisible(false);
+      setSelectedDate(selectedDate);
+      let tempDate = new Date(selectedDate);
+      let choppedDate = tempDate.getDate() + '/' + (tempDate.getMonth() +1) + '/' + tempDate.getFullYear();
+      setDateText(choppedDate);
+      setValue('dueDate', choppedDate);
+    }
+  }
+    
+  const onSubmit = (data) => console.log(data)
+  
   return (
     <Modal
       animationType="none"
@@ -24,43 +65,121 @@ const ToDoModal = ({ todoModalVisible, setToDoModalVisible }) => {
         </View>
         <View style={styles.top}>
           <Button title="Back" onPress={() => setToDoModalVisible(!todoModalVisible)} />
-          <Text style={styles.create}>CREATE</Text>
+          <TouchableOpacity
+            onPress={handleSubmit(onSubmit)}>
+            <Text style={styles.create}>CREATE</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.main}>
           <View style={styles.cont}>
             <Text style={styles.text}>TITLE</Text>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <View>
+                  <TextInput
+                  placeholder=''
+                  onChangeText={onChange}
+                  value={value}
+                />
+                {errors.title && (
+                  <Text style={styles2.errorText}>{errors.title.message}</Text>
+                )}
+                </View>
+              )}
+              name="title"
+            />
           </View>
-          <View style={styles.cont}>
+          {/* <View style={styles.cont}>
             <Text style={styles.text}>NOTES</Text>
-          </View>
-          <View style={styles.posNeg}>
-            <View style={styles.PosCircle}>
-              <Text style={styles.text}>+</Text>
-            </View>
-            <View style={styles.NegCircle}>
-              <Text style={styles.text}>-</Text>
-            </View>
-          </View>
-          <Text style={styles.text}>DIFFICULTY</Text>
-          <View style={styles.difficultyOption}>
-            <View style={styles.easyDifficultyBox}>
-              <Text style={styles.text}>EASY</Text>
-            </View>
-            <View style={styles.mediumDifficultyBox}>
-              <Text style={styles.text}>MEDIUM</Text>
-            </View>
-            <View style={styles.HardDifficultyBox}>
-              <Text style={styles.text}>HARD</Text>
-            </View>
-          </View>
-          <View style={styles.cont}>
-            <Text style={styles.text}>Due Date:</Text>
-          </View>
+            <Controller
+              control={control}
+              rules={{
+                required: false,
+              }}
+              render={({ field: { onChange, value } }) => (
+              <TextInput
+                placeholder=''
+                onChangeText={onChange}
+                value={value}
+              />
+              )}
+              name="notes"
+            />
+          </View> */}
+          {/* <Text style={styles.text}>DIFFICULTY</Text>
+          {errors.difficulty && (
+                <Text style={styles2.errorText}>{errors.difficulty.message}</Text>
+              )}
+              <Controller
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field: { onChange, value } }) => (
+                <View style={styles.difficultyOption}>
+                    <TouchableOpacity
+                    style={styles.easyDifficultyBox}
+                    onPress={() => {
+                      onChange('easy')
+                    }}
+                    >
+                    <Text style={styles.text}>EASY</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.mediumDifficultyBox}
+                      onPress={() => {
+                        onChange('medium')
+                      }}
+                    >
+                    <Text style={styles.text}>MEDIUM</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.HardDifficultyBox}
+                      onPress={() => {
+                        onChange('hard')
+                      }}
+                    >
+                    <Text style={styles.text}>HARD</Text>
+                    </TouchableOpacity>
+                </View>
+                )}
+                name="difficulty"
+              /> */}
+          {/* <View style={styles.cont} >
+            <TouchableOpacity onPress={() => setCalendarVisible(true)}>
+              <Text style={styles.text}>Due Date: </Text>
+            </TouchableOpacity>
+            {calendarVisible && (
+              <DateTimePicker 
+                value={selectedDate}
+                mode='date'
+                is24Hour={true}
+                locale="fi-FI"
+                timeZoneName={'Europe/Helsinki'}
+                display='default'
+                onChange={calendarAction}
+              />
+              )}
+              {errors.date && !dateText ? (
+                <Text style={styles2.errorText}>{errors.date.message}</Text>
+              ) : (
+                <Text style={styles.text}>{dateText}</Text>
+              )}
+          </View> */}
         </View>
-
       </View>
     </Modal>
   );
 };
+
+const styles2 =  StyleSheet.create ({
+  errorText: {
+    color: '#AE0000'
+  }
+});
 
 export default ToDoModal;
