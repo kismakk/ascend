7f97475726aa9b1e2282542d7dc08cda0bf9ad98
kismakk <kismakk.code@gmail.com>
2024-04-18 13:09:51 +0300

    Add functionality to edit username
    - Settings screen now includes form to edit username
    - Changes are saved to the user signed in, can be accesed from `user.displayName`
    
    Additional changes:
    - Now, when creating a new account, start of the email is assigned as the username to prevent null values in `displayName`

--- hooks/useFirebaseAuth.js
+++ hooks/useFirebaseAuth.js
@@ -4,6 +4,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   deleteUser,
+  updateProfile,
 } from '../firebase/config';
 
 /**
@@ -33,6 +34,7 @@ import {
  */
 export default function useFirebaseAuth() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [authError, setAuthError] = useState(null);
 
   /**
@@ -126,6 +128,9 @@ export default function useFirebaseAuth() {
       .then((userCredential) => {
         const user = userCredential.user;
         handleUser(user);
+        updateUserInformation({
+          username: email.split('@')[0],
+        });
       })
       .catch((error) => handleError(error.code));
   };
@@ -143,8 +148,27 @@ export default function useFirebaseAuth() {
       .catch((error) => handleError(error.code));
   };
 
+  const updateUserInformation = (data) => {
+    clearError();
+    setLoading(true);
+    const profileData = {
+      ...(data.username && { displayName: data.username }),
+      ...(data.avatarUrl && { photoURL: data.avatarUrl }),
+    };
+
+    updateProfile(auth.currentUser, profileData)
+      .then(() => {
+        console.log('Profile updated succesfully');
+        setLoading(false);
+      })
+      .catch((error) => {
+        handleError(error.code);
+        setLoading(false);
+      });
+  };
+
   const deleteUserData = () => {
-    const user = auth.currentUser
+    const user = auth.currentUser;
 
     deleteUser(user)
       .then(() => {
@@ -154,7 +178,16 @@ export default function useFirebaseAuth() {
         // An error ocurred
         // ...
       });
-  }
+  };
 
-  return { user, authError, signIn, signOut, signUp, deleteUserData };
+  return {
+    user,
+    authError,
+    loading,
+    signIn,
+    signOut,
+    signUp,
+    deleteUserData,
+    updateUserInformation,
+  };
 }
